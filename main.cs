c8cc5b5ce5b0e13d03baf07618a9739ff36bd5db
ArthurTuggle <arthurtuggle57@yahoo.com>
2020-12-07 16:19:18 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,21 @@
+using System;  
+  public class speed  
+   {  
+     public static void Main(string[] args)  
+      { 
+           int i;
+           double distance=0,time=0,speed=0,average=0 ,total=0;    
+           for(i = 1; i <= 7; i++)    
+          {
+             Console.Write("\nEnter the distance covered by cars #"+i);    
+             distance = double.Parse(Console.ReadLine()); 
+             Console.Write("\nEnter the time taken by car #"+i);    
+             time = double.Parse(Console.ReadLine()); 
+             speed=  distance/time; 
+             total+=speed;
+             Console.Write("\nThe speed of this car is "+ speed +" miles per hour");    
+          }
+          average =total/7;
+          Console.Write("\nThe average  speed of the cars is "+ average +"miles per hour");    
+     }  
+   }
